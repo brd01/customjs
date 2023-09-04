9ab7b553d6850069082be867982bb398a8f0c30c
brd01 <144014468+brd01@users.noreply.github.com>
2023-09-04 13:49:54 +0300

    Update av.js

--- av.js
+++ av.js
@@ -4,7 +4,7 @@
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-T5WPCKW');
-//End Google Tag Manager
+//End Google Tag Managerzz
 
 
 //Facebook Pixel Code
