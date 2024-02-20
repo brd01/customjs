0cae9031bf015f85efeefeb631faf1f9d4e86d25
brd01 <144014468+brd01@users.noreply.github.com>
2024-02-20 15:25:22 +0300

    Update av.js

--- av.js
+++ av.js
@@ -12,9 +12,9 @@
 		z.innerHTML = '<div id="mqdisplay"><div class="marquee"><div class="img1"><img class="mqimg" src="https://cmsbetconstruct.com/storage/medias/avvabet-18751834/media_18751834_de1a139f723fe0220ddf23bf4b83ab16.png" alt=""> Güncel adresimiz <span><span>www.xxx.com</span></span>\'dur. giriş adresi her zaman sıralı olarak ilerlemektedir. <img class="mqimg" src="https://cmsbetconstruct.com/storage/medias/Lejonbet/media_706_e76be44005b0f8defd523c4a31c6596b.png" alt=""> Pagcor lisansı kalite ve güvencesi! <img class="mqimg" src="https://cmsbetconstruct.com/storage/medias/avvabet-18751834/media_18751834_de1a139f723fe0220ddf23bf4b83ab16.png" alt=""> ..</div> </div></div>';
 		document.querySelector('.layout-header-holder-bc').appendChild(z);
 
-		var lz = document.createElement('div');
-		lz.innerHTML = '<a target="_blank" rel="nonoopener" href="https://validator-antillephone.com/validate?domain=www.avvabet459.com&seal_id=b8168b8aef012ae1238574dc074a2cb3ee35ce0044f3e031ffdc72f47d7b8b1b8087fa8e83302f71940fefb90dcb532ba&stamp=7def1c4f06e53aa44a393d357c047532"><img alt="" style="width: 100%; height: auto" src="https://validator-antillephone.com/fab2abba25c5ab955bd925cb80e096a3-www.avvabet459"></a>';		  
-		document.querySelector('#seal-31eb72b0-a649-4ffc-8de4-aacec6031a71').appendChild(lz);
+		
+		lz = '<a target="_blank" rel="nonoopener" href="https://validator-antillephone.com/validate?domain=www.avvabet459.com&seal_id=b8168b8aef012ae1238574dc074a2cb3ee35ce0044f3e031ffdc72f47d7b8b1b8087fa8e83302f71940fefb90dcb532ba&stamp=7def1c4f06e53aa44a393d357c047532"><img alt="" style="width: 100%; height: auto" src="https://validator-antillephone.com/fab2abba25c5ab955bd925cb80e096a3-www.avvabet459"></a>';		  
+		document.querySelector('#seal-31eb72b0-a649-4ffc-8de4-aacec6031a71').innerHTML=lz;
 		  
 			
 
@@ -51,6 +51,13 @@
 		var z = document.createElement('div');
 		z.innerHTML = '<div id="mqdisplay"><div class="marquee"><div class="img1"><img class="mqimg" src="https://cmsbetconstruct.com/storage/medias/avvabet-18751834/media_18751834_de1a139f723fe0220ddf23bf4b83ab16.png" alt=""> Güncel adresimiz <span><span>www.xxx.com</span></span>\'dur. giriş adresi her zaman sıralı olarak ilerlemektedir. <img class="mqimg" src="https://cmsbetconstruct.com/storage/medias/Lejonbet/media_706_e76be44005b0f8defd523c4a31c6596b.png" alt=""> Pagcor lisansı kalite ve güvencesi! <img class="mqimg" src="https://cmsbetconstruct.com/storage/medias/avvabet-18751834/media_18751834_de1a139f723fe0220ddf23bf4b83ab16.png" alt=""> .. </div> </div></div>';
 		document.querySelector('.header-bc').appendChild(z);
+
+
+			
+		lz = '<a target="_blank" rel="nonoopener" href="https://validator-antillephone.com/validate?domain=www.avvabet459.com&seal_id=b8168b8aef012ae1238574dc074a2cb3ee35ce0044f3e031ffdc72f47d7b8b1b8087fa8e83302f71940fefb90dcb532ba&stamp=7def1c4f06e53aa44a393d357c047532"><img alt="" style="width: 100%; height: auto" src="https://validator-antillephone.com/fab2abba25c5ab955bd925cb80e096a3-www.avvabet459"></a>';		  
+		document.querySelector('#seal-31eb72b0-a649-4ffc-8de4-aacec6031a71').innerHTML=lz;
+		  
+			
 			
 	  }
 
