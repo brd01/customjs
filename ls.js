b20c56249ddfa7e01ba54d595de63ebde0374527
brd01 <144014468+brd01@users.noreply.github.com>
2025-03-25 10:32:39 -0700

    Update ls.js

--- ls.js
+++ ls.js
@@ -1,1 +1,9 @@
-//File24
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-46Y6M1RE55"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-46Y6M1RE55');
+</script>
